1f401751a37b5ac06ce4ffdb460fe2907d455c63
Andreus-coder <55553397+Andreus-coder@users.noreply.github.com>
2023-01-04 14:14:45 -0300

    feat: Form Schema

---
+++ src/components/form/formSchemas.ts
@@ -0,0 +1,43 @@
+import * as yup from "yup";
+
+//regexp will validate the discord username#tag
+const regexp = new RegExp('^.{3,32}#[0-9]{4}$');
+
+export const loginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .required("O e-mail é obrigatório.")
+    .email("O e-mail preenchido é inválido."),
+  password: yup
+    .string()
+    .required("A senha é obrigatória.")
+    .min(6, "A senha deve conter 6 characteres."),
+});
+
+export const registerSchema = yup.object().shape({
+  name: yup
+    .string()
+    .required("O nome é obrigatório.")
+    .min(5, "O nome deve conter ao menos 5 digitos"),
+  email: yup
+    .string()
+    .required("O e-mail é obrigatório.")
+    .email("O e-mail preenchido é inválido."),
+  password: yup
+    .string()
+    .required("A senha é obrigatória.")
+    .min(6, "A senha deve conter 6 characteres."),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "As senhas devem ser iguais.")
+    .required("Confirme a senha.")
+    .min(6, "A senha deve conter 6 characteres."),
+  socialMedia: yup
+    .string()
+    .nullable()
+    .notRequired()
+    .when("socialMedia", {
+      is: (value: string) => value?.length,
+      then: (rule) => rule.matches(regexp),
+    }),
+});
