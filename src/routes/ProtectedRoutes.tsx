36a26a24d40922d010b8740c893ed20c294b97f6
Andreus-coder <55553397+Andreus-coder@users.noreply.github.com>
2023-01-09 17:37:45 -0300

    feat: ProtectedRoutes

--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { UserContext } from "../contexts/UserContext/UserContext";
 
 export const ProtectedRoutes = () => {
   const { user } = useContext(UserContext);
