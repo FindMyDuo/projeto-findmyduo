cf283f49eca8d4fda0500a91fcd17709a60fa535
Andreus-coder <55553397+Andreus-coder@users.noreply.github.com>
2023-01-11 19:16:59 -0300

    despair: git driving me crazy

--- src/components/navbar/mobileNavbar.tsx
+++ src/components/navbar/mobileNavbar.tsx
@@ -7,19 +7,17 @@ import { ModalProfileUser } from "../ModalUserProfile/modalUserProfile";
 import { StyledMobileNavbar } from "./styles";
 import { StyledChatIcon, StyledHomeIcon, StyledUserIcon } from "./styledIcons";
 import ChatFeed from "../chat/feed/ChatFeed";
-<<<<<<< HEAD
 import { NavContext } from "../../contexts/NavContext/NavContext";
-=======
 import { UserCard } from "../card/userCard/UserCard";
->>>>>>> 4ee13c1ebca45ebd8db50138195b407271fad755
 
 export const MobileNavBar = () => {
   const { user } = useContext(UserContext);
   const { nav, setNav } = useContext(NavContext);
 
   function handleClick(e: React.MouseEvent) {
-    const parent: HTMLElement | null | undefined = (e.target as HTMLButtonElement).parentElement
-      ?.parentElement;
+    const parent: HTMLElement | null | undefined = (
+      e.target as HTMLButtonElement
+    ).parentElement?.parentElement;
     if (nav !== e.target.value && e.target.value !== "") {
       if (parent.classList.contains("slide-down-navbar")) {
         parent.classList.remove("slide-down-navbar");
@@ -89,14 +87,9 @@ export const MobileNavBar = () => {
             </button>
           )}
         </div>
-<<<<<<< HEAD
         {nav === "profile" ? <ModalProfileUser user={user} /> : null}
         {nav === "chat" ? <ChatFeed /> : null}
-=======
-        {button === "profile" ? <ModalProfileUser user={user} /> : null}
-        {button === "chat" ? <ChatFeed /> : null}
-        {button === "users" ? <UserCard /> : null}
->>>>>>> 4ee13c1ebca45ebd8db50138195b407271fad755
+        {nav === "users" ? <UserCard /> : null}
       </StyledMobileNavbar>
     </>
   );
